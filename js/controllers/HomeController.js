f0334434a1d86a85a7b3f435cce67c83d83484f7
alexcutschall <alexander.cutschall@gmail.com>
2018-07-01 09:42:52 -0600

    a user can now add a new suggestion

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -3,4 +3,18 @@ app.controller("HomeController", ["$scope",
  function($scope, suggestions) {
   $scope.helloWorld = "Hello World";
   $scope.posts = suggestions.posts;
+
+  $scope.addSuggestion = function() {
+
+    if ($scope.title == false || $scope.title == '') {
+      return;
+    }
+
+    $scope.posts.push({
+      title: $scope.title,
+      upvotes: 0
+    });
+
+    $scope.title = '';
+  };
 }])
